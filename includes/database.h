78a0a0a6370b8eecf370da940089d46647d7e281
Nathan Oliveira <nathanoliveira399@gmail.com>
2023-12-02 12:01:41 -0300

    feat: modelagem do banco de dados que armazenará as tabelas

---
+++ includes/database.h
@@ -0,0 +1,18 @@
+#ifndef DATABASE_H
+#define DATABASE_H
+
+#include <stddef.h>
+#include "sql_table.h"
+
+typedef struct database DATABASE;
+
+struct database {
+    SQL_TABLE*  table;
+    size_t      storage;
+    size_t      n_tables;
+};
+
+DATABASE* initialize(size_t storage);
+void add_table(DATABASE* db, SQL_TABLE* tb);
+
+#endif
