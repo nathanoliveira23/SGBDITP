9bad9c274a0819a2cce80f7018c18ff26e2787f9
Nathan Oliveira <nathanoliveira399@gmail.com>
2023-12-02 09:31:55 -0300

    feat: modelagem da table sql

---
+++ includes/sql_table.h
@@ -0,0 +1,39 @@
+#ifndef SQL_TABLE_H
+#define SQL_TABLE_H
+
+#include <stddef.h>
+#include <stdbool.h>
+
+#define STRLEN 50
+
+enum dt { INT, FLOAT, CHAR, STRING, BOOL };
+
+typedef enum dt             DATATYPE;
+typedef struct column       COLUMN;
+typedef union col_datatype  COL_DATATYPE;
+typedef struct table        SQL_TABLE;
+
+union col_datatype {
+    char    *char_datatype;
+    char    **string_datatype;
+    int     *int_datatype;
+    float   *float_datatype;
+    bool    *bool_datatype;
+};
+
+struct column {
+    char            col_name[STRLEN];
+    bool            is_PK;
+    unsigned        PK;
+    size_t          col_lines;
+    COL_DATATYPE    data_type;
+};
+
+struct table {
+    char    tb_name[STRLEN];
+    size_t  tb_cols;
+    bool    has_PK;
+    COLUMN  *column;
+};
+
+#endif
