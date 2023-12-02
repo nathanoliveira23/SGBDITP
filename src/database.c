2c42ecbc50aa590db7e59315fd9bb21332b2d32b
Nathan Oliveira <nathanoliveira399@gmail.com>
2023-12-02 17:35:05 -0300

    feat: add fuções de criação/gerenciamento de banco de dados

---
+++ src/database.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include "../includes/database.h"
+
+DATABASE* initialize(size_t storage)
+{
+    DATABASE* db = malloc(sizeof(DATABASE));
+
+    db->storage = storage;
+    db->n_tables = 0;
+    db->table = malloc(storage * sizeof(SQL_TABLE));
+
+    return db;
+}
+
+void add_table(DATABASE* db, SQL_TABLE* tb)
+{
+    if (db->n_tables < db->storage)
+        db->table[db->n_tables++] = *tb;
+    else {
+        db->storage *= 2;
+        db->table = realloc(db->table, db->storage);
+        db->table[db->n_tables++] = *tb;
+    }
+}
