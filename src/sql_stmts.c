f77b431da5a39e739f1dac7bf32f069d07ec9750
Nathan Oliveira <nathanoliveira399@gmail.com>
2023-12-03 19:48:54 -0300

    feat: implementação da função de inserção de dados em uma coluna

--- src/sql_stmts.c
+++ src/sql_stmts.c
@@ -16,19 +16,44 @@ SQL_TABLE* create_table(char tbname[STRLEN], size_t ncols)
     return tb;
 }
 
-void add_column(DATABASE* db, char tbname[STRLEN], char colname[STRLEN], DATATYPE dt, bool isPK)
+SQL_TABLE* find_table_by_name(const DATABASE* db, const char* tbname)
 {
     SQL_TABLE* tb_target;
-    bool found_table;
+    bool found;
 
     for (size_t i = 0; i < db->n_tables; i++) {
-        found_table = strcmp(db->table[i].tb_name, tbname) == 0;
+        found = strcmp(db->table[i].tb_name, tbname) == 0;
 
-        if (found_table) {
+        if (found) {
             tb_target = &(db->table[i]);
             break;
         }
     }
+
+    return tb_target;
+}
+
+COLUMN* find_column_by_name(const DATABASE *db, const char* tbname, const char *colname)
+{
+    SQL_TABLE* tb = find_table_by_name(db, tbname);
+    COLUMN* col_target;
+    bool found;
+
+    for (size_t i = 0; i < tb->tb_cols; i++) {
+        found = strcmp(tb->column[i].col_name, colname) == 0;
+
+        if (found) {
+            col_target = &(tb->column[i]);
+            break;
+        }
+    }
+
+    return col_target;
+}
+
+void add_column(DATABASE* db, char tbname[STRLEN], char colname[STRLEN], DATATYPE dt, bool isPK)
+{
+    SQL_TABLE* tb_target = find_table_by_name(db, tbname);
 
     strcpy(tb_target->column->col_name, colname);
 
@@ -39,23 +64,28 @@ void add_column(DATABASE* db, char tbname[STRLEN], char colname[STRLEN], DATATYP
         case INT:
             if (isPK)
                 tb_target->column->is_PK = true;
-
+            
+            tb_target->column->typeof_column = INT;
             tb_target->column->data_type.int_datatype = calloc(10, sizeof(int));
 
             break;
         case FLOAT:
+            tb_target->column->typeof_column = FLOAT; 
             tb_target->column->data_type.float_datatype = calloc(10, sizeof(float));
             break;
         case CHAR:
+            tb_target->column->typeof_column = CHAR;
             tb_target->column->data_type.char_datatype = calloc(10, sizeof(char));
             break;
         case STRING:
+            tb_target->column->typeof_column = STRING;
             tb_target->column->data_type.string_datatype = calloc(10, sizeof(char*));
 
             for (size_t i = 0; i < 10; i++)
                 tb_target->column->data_type.string_datatype[i] = calloc(STRLEN, sizeof(char));
             break;
         case BOOL:
+            tb_target->column->typeof_column = BOOL;
             tb_target->column->data_type.bool_datatype = calloc(10, sizeof(bool));
             break;
         default:
@@ -63,6 +93,35 @@ void add_column(DATABASE* db, char tbname[STRLEN], char colname[STRLEN], DATATYP
     }
     
     db->table->tb_cols++;
+}
 
-    // adicionar tabela ao banco de dados
+void insert_into(DATABASE *db, char *tbname, char *colname, void *data)
+{
+    SQL_TABLE* tb = find_table_by_name(db, tbname);
+    COLUMN* col = find_column_by_name(db, tbname, colname);
+
+    switch (col->typeof_column) {
+        case INT:
+            col->data_type.int_datatype[col->col_lines++] = *(int*) data;
+            break;
+        case FLOAT:
+            col->data_type.float_datatype[col->col_lines++] = *(float*) data;
+            break;
+        case CHAR:
+            col->data_type.char_datatype[col->col_lines++] = *(char*) data;
+            break;
+        case STRING:
+            col->data_type.string_datatype[col->col_lines++] = *(char**) data;
+            break;
+        case BOOL:
+            col->data_type.bool_datatype[col->col_lines++] = *(bool*) data;
+            break;
+        default:
+            printf("Este tipo de dado e incompativel com a coluna %s.\n", col->col_name);
+    }
+}
+
+void select_all_tables(const DATABASE *db)
+{
+
 }
