503d56e2d8cd7ea136d8158dc9673943292eed25
Nathan Oliveira <nathanoliveira399@gmail.com>
2023-12-03 19:48:12 -0300

    refact: refatoração dos parâmetros das instruções SQL e adição de funções de busca

--- includes/sql_stmts.h
+++ includes/sql_stmts.h
@@ -4,9 +4,12 @@
 #include "database.h"
 #include "sql_table.h"
 
-SQL_TABLE* create_table(char tb_name[STRLEN], size_t ncols);
-void add_column(DATABASE* db, char tbname[STRLEN], char colname[STRLEN], DATATYPE dt, bool isPK);
-void insert_into(DATABASE* db, char tbname[STRLEN], char colname[STRLEN], void* data);
+SQL_TABLE* find_table_by_name(const DATABASE* db, const char* tbname);
+COLUMN* find_column_by_name(const DATABASE* db, const char* tbname, const char* colname);
+
+SQL_TABLE* create_table(const char tb_name[STRLEN], size_t ncols);
+void add_column(DATABASE* db, const char tbname[STRLEN], const char colname[STRLEN], DATATYPE dt, bool isPK);
+void insert_into(DATABASE* db, const char tbname[STRLEN], const char colname[STRLEN], void* data);
 void select_all_tables(const DATABASE* db);
 
 #endif
